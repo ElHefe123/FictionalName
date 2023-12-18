88f407030a8b036a0afa476579bf25a1039b8308
ElHefe123 <142462363+ElHefe123@users.noreply.github.com>
2023-12-17 20:52:54 -0500

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string firstName = "George";
+    string lastName = "The Monkey";
+    string streetAddress = "111222 Banana Road";
+    string city = "Hopscotch City";
+    string zipCode = "34598";
+
+    cout << "Default User Information:\n";
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
